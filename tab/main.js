d5653618a78d8253e989900df6fffb4dea52b191
Oohira Suguru <suguru.13579515@gmail.com>
2021-11-22 21:42:01 +0900

    Study JavaScript

---
+++ tab/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+{
+  const tabLabels = document.querySelectorAll('.tab__label li a');
+  const tabContents = document.querySelectorAll('.tab__content');
+  
+  tabLabels.forEach( clickedLabel => {
+    clickedLabel.addEventListener('click',e => {
+      console.log(tabLabels);
+      e.preventDefault();
+  
+      tabLabels.forEach(label => {
+        label.classList.remove('active');
+      });
+  
+      clickedLabel.classList.add('active');
+  
+      tabContents.forEach(content => {
+        content.classList.remove('active');
+      });
+  
+      document.getElementById(clickedLabel.dataset.id).classList.add('active');
+    });
+  });
+}  
